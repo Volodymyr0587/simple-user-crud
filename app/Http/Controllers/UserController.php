9f0d764009e53b5386d74af4938a335b7e04b67e
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-07-30 12:44:30 +0300

    05 - Log In & Log Out

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,15 +10,38 @@ class UserController extends Controller
     public function register(Request $request)
     {
         $formFields = $request->validate([
-            'name' => 'required|min:3|max:10',
-            'email' => 'required|email',
+            'name' => 'required|min:3|max:10|unique:users', // Rule::unique('usres', 'name)
+            'email' => 'required|email|unique:users',
             'password' => 'required|min:8|max:200'
         ]);
 
         $formFields['password'] = bcrypt($formFields['password']);
 
-        User::create($formFields);
+        $user = User::create($formFields);
 
-        return 'Hello from UserController :)';
+        auth()->login($user);
+
+        return redirect('/');
+    }
+
+    public function login(Request $request)
+    {
+        $formFields = $request->validate([
+            'loginemail' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if (auth()->attempt(['email' => $formFields['loginemail'], 'password' => $formFields['loginpassword']])) {
+            $request->session()->regenerate();
+        }
+
+        return redirect('/');
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return redirect('/');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,58 +12,119 @@
 </head>
 
 <body>
-    <div class="w-full max-w-xs">
-        <form action="/register" method="POST" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    @auth
+        <p class="m-4">Congrats you are logged in.</p>
+        <form action="/logout" method="POST" class="m-4">
             @csrf
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
-                    Name
-                </label>
-                <input
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    name="name" type="text" placeholder="Name">
-                <div class="p-2">
-                    @error('name')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-                    Email
-                </label>
-                <input name="email"
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="email" type="text" placeholder="Email">
-                <div class="p-2">
-                    @error('email')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                    Password
-                </label>
-                <input name="password"
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="password" type="password" placeholder="Password">
-                <div class="p-2">
-                    @error('password')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-            </div>
-
             <div class="flex items-center justify-between">
                 <button
                     class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                     type="submit">
-                    Register
+                    Logout
                 </button>
             </div>
         </form>
-    </div>
+    @else
+        <div class="w-full max-w-xs">
+
+            <form action="/register" method="POST" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                @csrf
+
+                <h2 class="mt-2">Register</h2>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                        Name
+                    </label>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        name="name" type="text" placeholder="Name">
+                    <div class="p-2">
+                        @error('name')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                        Email
+                    </label>
+                    <input name="email"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email" type="text" placeholder="Email">
+                    <div class="p-2">
+                        @error('email')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input name="password"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password" type="password" placeholder="Password">
+                    <div class="p-2">
+                        @error('password')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="flex items-center justify-between">
+                    <button
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit">
+                        Register
+                    </button>
+                </div>
+            </form>
+        </div>
+
+        <div class="w-full max-w-xs">
+
+            <form action="/login" method="POST" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                @csrf
+
+                <h2 class="m-4 text-lg">Login</h2>
+
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="loginemail">
+                        Email
+                    </label>
+                    <input name="loginemail"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="loginemail" type="text" placeholder="Email">
+                    <div class="p-2">
+                        @error('loginemail')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="loginpassword">
+                        Password
+                    </label>
+                    <input name="loginpassword"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="loginpassword" type="password" placeholder="Password">
+                    <div class="p-2">
+                        @error('loginpassword')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="flex items-center justify-between">
+                    <button
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit">
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
+    @endauth
 </body>
 
 </html>
